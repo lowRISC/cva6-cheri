5a63075b71f875ba66927752a1ed3f698cc2f8f4
Marno van der Maas <mvdmaas+git@lowrisc.org>
2026-04-17 13:40:15 +0100

    [id] Bug already fixed upstream to id_stage
    Off by one error with number of issue stages

--- core/id_stage.sv
+++ core/id_stage.sv
@@ -166,7 +166,7 @@ module id_stage #(
   logic                                               commit_redirect_q;
 
   assign int_mode_decode[0] = int_mode_q;
-  for (genvar i = 1; i <= CVA6Cfg.NrIssuePorts; i++) begin
+  for (genvar i = 1; i < CVA6Cfg.NrIssuePorts; i++) begin
     assign int_mode_decode[i] = int_mode_decode_o[i-1];
   end
 
@@ -472,7 +472,7 @@ module id_stage #(
     if (commit_redirect_q) int_mode_d = int_mode_issue_i;
     else if (mispredict_redirect_i) int_mode_d = int_mode_resolved_branch_i;
     else begin
-      for (int i = 0; i <= CVA6Cfg.NrIssuePorts; i++) begin
+      for (int i = 0; i < CVA6Cfg.NrIssuePorts; i++) begin
         if (fetch_entry_ready_o[i]) begin
           int_mode_d = int_mode_decode_o[i];
         end
